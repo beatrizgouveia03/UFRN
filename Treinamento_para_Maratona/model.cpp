87d5c0af44fb3e186c56462661a0c61ad6cec7ab
beatriz <beatrizgouveia.gadelha0@gmail.com>
2026-04-15 13:38:24 -0300

    feat: modified model

--- Treinamento_para_Maratona/model.cpp
+++ Treinamento_para_Maratona/model.cpp
@@ -3,14 +3,17 @@
 using namespace std;
 
 #define endl '\n'
-#define f first
-#define s second
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 #define in cin >>
 #define out cout << 
 #define end << endl
-#define ii pair<int,int>
+#define pi pair<int,int>
 #define vi vector<int>
 #define vb vector<bool>
+#define REP(i,a,b) for(auto i{a}; i<b; i++)
 #define precise fixed << setprecision(4)
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
 
