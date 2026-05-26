8018323869208a22aa730477fdf222ad8106fcb1
beatriz <beatrizgouveia.gadelha0@gmail.com>
2026-05-26 09:53:55 -0300

    restored solution A from Data Structure Contest

--- Treinamento_para_Maratona/Treinamento MFP 26/Estrutura de Dados/A.cpp
+++ Treinamento_para_Maratona/Treinamento MFP 26/Estrutura de Dados/A.cpp
@@ -1,1 +1,38 @@
-6972A84B3BA257242688FF5D00619EC76972A84B3BA257242688FF5D00619EC7
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define in cin >>
+#define out cout << 
+#define end << endl
+#define pi pair<int,int>
+#define vi vector<int>
+#define vb vector<bool>
+#define REP(i,a,b) for(auto i{a}; i<b; i++)
+#define precise fixed << setprecision(4)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+
+typedef unsigned long long int ll;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+signed main(){ _ 
+   ll m, n, a;
+   in n >> m >> a;
+
+   ll x, y;
+
+   x = n/a;
+   y = m/a;
+
+   if( n%a != 0 ) x++;
+   if( m%a != 0 ) y++;
+   
+   out (x*y) end;   
+}
